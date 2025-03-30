f9abd7c18a3da3cf68236aed5ac5cca4a6c5e891
Ujjwal Thakuri <Ujjwalthakuri46@gmail.com>
2025-03-30 12:47:06 +0545

    day 35

--- restro/urls.py
+++ restro/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('category_list/', category_list)
+    path('category/', category_list),
+    path('category/<pk>', Category_detail)
 ]
--- restro/views.py
+++ restro/views.py
@@ -2,12 +2,34 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import *
 from .serializer import *
 # Create your views here.
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def category_list(request):
-    category = Category.objects.all()
-    serializer = CategorySerializer(category, many=True)
-    return Response (serializer.data)   
+    if request.method == "GET":
+        category = Category.objects.all()
+        serializer = CategorySerializer(category, many=True)
+        return Response (serializer.data)
+           
+    elif request.method =="POST":
+        serializer = CategorySerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response (serializer.data)
+    
+@api_view(['GET', 'DELETE'])
+def Category_detail(request, pk):
+    category = Category.objects.get(pk=pk)
+    if request.method == 'GET' :
+        serializer = CategorySerializer(category)
+        return Response (serializer.data)
+    elif request.method == 'DELETE':
+        # order_item = order_item.objects.filter(Food__Category = category).count()
+        # if order_item>0:
+        #     raise ValidationError({'details': 'CANNOT DELETE'})
+        category.delete()
+        return Response({"detail": "Data deleted"}, status= status.HTTP_204_No_CONTENT)
+        
